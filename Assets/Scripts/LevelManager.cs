e54bacb44cfe02037a9c954638f649461ae7bb07
Kevin Kendle <kevin.kendle09@gmail.com>
2016-10-01 14:14:58 -0500

    Scene setup and temp buttons to go through levels.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// temp function to see that level changing works
+	void Start() {
+		Debug.Log("Current level is " + Application.loadedLevel);
+	}
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested for: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log("I want to quit");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	static MusicPlayer instance = null;
+
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip bossClip;
+	public AudioClip endClip;
+
+	private AudioSource music;
+
+	void Start() {
+		if (instance != null && instance != this) {
+			Destroy(gameObject);
+			Debug.Log("Duplicate music player found. Destroying");
+		}
+		else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+			// uncomment when we have music
+//			music = GetComponent<AudioSource>();
+//
+//			Debug.Log("Playing beginning of menu music");
+//			music.clip = startClip;
+//			music.loop = true;
+//			music.Play();
+		}
+	}
+
+	void OnLevelWasLoaded(int level) {
+		Debug.Log("music player loaded level " + level);
+
+		// play different music on different levels
+//		if(level == 1) {
+//			Debug.Log("Playing general game music");
+//			music.Stop();
+//			music.clip = gameClip;
+//			music.loop = true;
+//			music.Play();
+//		}
+//		if(level == 3 || level == 5 || level == 7 || level == 9 || level == 11 || level == 13) {
+//			Debug.Log("Continuing general game music");
+//			music.clip = gameClip;
+//		}
+//		if(level == 15) {
+//			Debug.Log("Switching to boss music");
+//			music.Stop();
+//			music.clip = bossClip;
+//			music.loop = true;
+//			music.Play();
+//		}
+//		if(level == 16 || level == 17) {
+//			Debug.Log("Switching to menu music");
+//			music.Stop();
+//			music.clip = endClip;
+//			music.loop = true;
+//			music.Play();
+//		}
+	}
+}
