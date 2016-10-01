7f726cfe61ecbcc955331e0cc5b4ccc19b45fded
Kevin Kendle <kevin.kendle09@gmail.com>
2016-10-01 17:08:41 -0500

    Add placeholder hero. Add hero movement. Restrict hero movement on x axis. Needs y axis restriction.

---
+++ Assets/Entities/Hero/Scripts/HeroController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+
+	public float speed;
+	public float padding = 1.0f;
+
+	float xmin;
+	float xmax;
+
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+		
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+	}
+
+	void Update () {
+		// restrict the hero to the gamespace
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
+		// hero movement
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		else if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		else if(Input.GetKey(KeyCode.UpArrow)) {
+		transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		else if(Input.GetKey(KeyCode.DownArrow)) {
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gbjam5
 gbjam 5
+
+# Attributions
+Game Art and Music
+http://opengameart.org/
+
+Hero
+isaiah658
+http://opengameart.org/content/green-cap-character-16x18
